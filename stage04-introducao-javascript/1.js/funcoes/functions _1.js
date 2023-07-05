81ac99a6a911a9a0105e9d16a16fdc2a6a774dd2
Flávio Félix <fau_felix30@hotmail.com>
2023-07-04 23:53:40 -0300

    Começando os estudos sobre functions

---
+++ stage04-introducao-javascript/1.js/funcoes/functions _1.js
@@ -0,0 +1,19 @@
+// Criar um aplicativo de frases motivacionais
+
+// declaration - declaração
+// funcition statement
+function createPhrases () {
+    console.log("Estudar é muito bom");
+    console.log("Paciência e Persistência");
+    console.log("Revisão é mãe do aprendizado");
+};
+
+// executar a função
+// rodar, chamar e invocar
+// execute, run and invoke
+createPhrases();
+createPhrases();
+createPhrases();
+createPhrases();
+
+console.log("Fim do programa");
---
+++ stage04-introducao-javascript/1.js/funcoes/functions.js
@@ -0,0 +1,17 @@
+// function expression
+// function anonymous
+
+// parâmetros (paremeters)
+const sum = function (number1, number2) {
+    let total = number1 + number2;
+    return total;
+};
+
+let number1 = 34;
+let number2 = 25;
+
+//console.log(`o número 1 e ${number1}`);
+//console.log(`o número 2 e ${number2}`);
+console.log(`a soma é  ${sum(number1, number2)}`);
+
+
