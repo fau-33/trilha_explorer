039e1fb880a85e2becc225b9ec09b7aa9c50b4ab
Flávio Félix <fau_felix30@hotmail.com>
2023-08-03 19:12:11 -0300

    Feito o problema sobre lista de compras

--- stage04-introducao-javascript/2.logica_estrturas/5.lista_compras/notes.md
+++ stage04-introducao-javascript/2.logica_estrturas/5.lista_compras/notes.md
@@ -4,4 +4,9 @@
 - Melhor eficiência no código
 
 [] Estrutura de repetição
+    - Laço de repetição
+    - for
 [] Vetores (array)
+    [] tipo de dado estruturado
+[] incremental (++): o valor + 1
+[] Tipo de dado: undefined
--- stage04-introducao-javascript/2.logica_estrturas/5.lista_compras/script.js
+++ stage04-introducao-javascript/2.logica_estrturas/5.lista_compras/script.js
@@ -4,3 +4,12 @@
   Após capturar os 10 items, imprima-os, separando por vírgula.
 */
 
+let items = [];
+
+for(let item = 0; item < 10; item++) {
+   let itemName = prompt("Digite o item " + (item + 1));
+   items[item] = itemName;
+
+}
+
+alert(items);
