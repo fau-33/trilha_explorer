fe3f1b39f79d4cce6a65fcf642a7e952c5aeb2b7
Flávio Félix <fau_felix30@hotmail.com>
2023-08-04 20:46:03 -0300

    Feito o exercício 01 desafio javascript

--- stage04-introducao-javascript/2.logica_estrturas/10.exerc1-soma/script.js
+++ stage04-introducao-javascript/2.logica_estrturas/10.exerc1-soma/script.js
@@ -9,8 +9,8 @@
 
 Fácil até aqui, né? Bora se desafiar? 👀 🧑‍🚀
 
-- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
-- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
+- [x]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [x]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
  */
 
 let numberOne = Number(prompt("Digite o primeiro número? "));
@@ -19,14 +19,26 @@ let numberTwo = Number(prompt("Digite o segundo numero? "));
 let sum = numberOne + numberTwo;
 alert("A soma dos dois números: " + sum);
 
+if(sum  % 2 === 0) {
+    alert("É um número par!")
+} else {
+    alert("É um numero ímpar!");
+}
+
 let subtraction = numberOne - numberTwo;
-alert("A subtração dos dois numeros: " + subtraction);
+alert(`A subtração dos dois numeros: ${subtraction}`);
 
 let multiplication = numberOne * numberTwo;
-alert("A multiplicação dos dois números; " + multiplication);
+alert(`A multiplicação dos dois números; ${multiplication}`);
 
 let division = numberOne / numberTwo;
-alert("A divisão dos dois números; " + division);
+alert(`A divisão dos dois números; ${division}`);
 
 let restOfDivision = numberOne % numberTwo;
-alert("O resto da divisão dos dois números; " + restOfDivision);
+alert(`O resto da divisão dos dois números; ${restOfDivision}`);
+
+if(numberOne === numberTwo) {
+    alert("Os números inseridos são iguais")
+} else {
+    alert("Os números inseridos são diferentes");
+}
