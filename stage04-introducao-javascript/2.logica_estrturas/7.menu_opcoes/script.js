de057b6cd19c1cd5a5e5747e8e6b8dc16248925e
Flávio Félix <fau_felix30@hotmail.com>
2023-08-04 16:17:08 -0300

    Feito o desafio menu de opções

--- stage04-introducao-javascript/2.logica_estrturas/7.menu_opcoes/script.js
+++ stage04-introducao-javascript/2.logica_estrturas/7.menu_opcoes/script.js
@@ -41,22 +41,25 @@ while(option != 3) {
   2. Mostrar itens cadastrados
   3. Sair do programa
   `));
-  
-  
-  if(option == 1) {
-     let item = prompt("Digite o nome do item");
-      items.push(item);
-  }else if(option == 2) {
-  
-      if(items.length === 0) {
-          alert("Não existem itens cadastrados");
-      } else {
-        alert(items);
-      }
-  } else {
-    alert('good bye!');
-  }
 
-  console.log(option, items);
+switch(option) {
+  case 1:
+    let item = prompt("Digite o nome do item");
+     items.push(item);
+    break;
+  case 2:
+    if(items.length === 0) {
+        alert("Não existem itens cadastrados");
+    } else {
+      alert(items);
+    }
+    break;
+  case 3:
+    alert('good bye!');
+    break;
+    default:
+     alert("Opção inválida.Tente novamente!"); 
+}  
+  
 }    
 
