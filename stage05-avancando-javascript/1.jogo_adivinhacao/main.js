80e3319f3f836375055976df1a17e32a54570937
Flávio Félix <fau_felix30@hotmail.com>
2023-08-09 20:35:09 -0300

    Feito o projeto jogo de adivinhação

--- stage05-avancando-javascript/1.jogo_adivinhacao/main.js
+++ stage05-avancando-javascript/1.jogo_adivinhacao/main.js
@@ -1,20 +1,44 @@
-const randomNumber = Math.round(Math.random() * 10);
-
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
-function handleClick(event) {
+// Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick();
+    }
+});
+
+// Funções
+function handleTryClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
+    toggleScreen();
 
-    document.querySelector(
-      ".screen2 h2"
+    screen2.querySelector(
+      "h2"
     ).innerText = `Acertou em ${xAttempts} tentativas`;
   }
 
+  inputNumber.value = "";
   xAttempts++;
-}
+};
+
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 1;
+  randomNumber = Math.round(Math.random() * 10);
+};
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+};
