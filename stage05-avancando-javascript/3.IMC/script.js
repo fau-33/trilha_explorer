a1983c6384faf76e1a27a5625686e2499007ce9d
Flávio Félix <fau_felix30@hotmail.com>
2023-09-01 21:45:42 -0300

    Feito a lógica do modal

--- stage05-avancando-javascript/3.IMC/script.js
+++ stage05-avancando-javascript/3.IMC/script.js
@@ -3,11 +3,26 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modalMessage = document.querySelector('.modal .title span');
+const modalBtnClose = document.querySelector('.modal button.close');
+
 form.onsubmit = (event) => {
     event.preventDefault();
 
     const weight = inputWeight.value;
     const height = inputHeight.value;
 
-    console.log(weight, height);
+    const result = IMC(weight, height);
+    const message = `Seu IMC é ${result}`;
+
+    modalMessage.innerText = message;
+    modalWrapper.classList.add('open');
 };
+
+modalBtnClose.onClick = () => modalWrapper.classList.remove('open');
+
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2);
+}
