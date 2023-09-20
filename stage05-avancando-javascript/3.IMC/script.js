598bc5b212f98e3f3979c2646ba0f43969801a16
Flávio Félix <fau_felix30@hotmail.com>
2023-09-20 13:52:03 -0300

    Estruturando os dados do Modal

--- stage05-avancando-javascript/3.IMC/script.js
+++ stage05-avancando-javascript/3.IMC/script.js
@@ -3,9 +3,20 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modalMessage = document.querySelector('.modal .title span');
-const modalBtnClose = document.querySelector('.modal button.close');
+
+const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open');
+    },
+    close() {
+        Modal.wrapper.classList.remove('open');
+    }
+}
 
 form.onsubmit = (event) => {
     event.preventDefault();
@@ -16,11 +27,13 @@ form.onsubmit = (event) => {
     const result = IMC(weight, height);
     const message = `Seu IMC é ${result}`;
 
-    modalMessage.innerText = message;
-    modalWrapper.classList.add('open');
+    Modal.message.innerText = message;
+
+    Modal.open();
 };
 
-modalBtnClose.onClick = () => modalWrapper.classList.remove('open');
+Modal.buttonClose.onClick = () => Modal.close();
+
 
 
 function IMC(weight, height) {
