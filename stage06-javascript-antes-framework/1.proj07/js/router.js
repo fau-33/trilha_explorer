7c8fd8975fa8a191a17db6a9c00b86af39c3c247
Flávio Félix <fau_felix30@hotmail.com>
2023-09-30 13:41:37 -0300

    Começando o router

---
+++ stage06-javascript-antes-framework/1.proj07/js/router.js
@@ -0,0 +1,28 @@
+class Router {
+
+    add(routeName, page) {
+        this.routes[routeName] = page
+    }
+
+    route(event) {
+        event.preventDefault()
+        event = event || window.event
+    
+        window.history.pushState({}, "", event.target.href)
+    
+        handle();
+    }
+
+    handle() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+    
+        fetch(route)
+        .then(data => data.text())
+        .then(html => {
+            document.querySelector('#app').innerHTML = html
+        })
+
+    }
+}
+
