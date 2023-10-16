e430591217e64701fc5c4dc3d3e3d9119efb4f78
Flávio Félix <fau_felix30@hotmail.com>
2023-10-16 17:50:20 -0300

    Feito a aula sobre promesas e async

--- stage06-javascript-antes-framework/2.github-favorites/js/Favorites.js
+++ stage06-javascript-antes-framework/2.github-favorites/js/Favorites.js
@@ -1,108 +1,117 @@
+export class GithubUser {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+    .then(data => data.json())
+    .then(({ login, name, public_repos, followers }) => ({
+      login,
+      name,
+      public_repos,
+      followers
+    }))
+  }
+}
+
+
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
 export class Favorites {
   constructor(root) {
-    this.root = document.querySelector(root);
-    this.load();
+    this.root = document.querySelector(root)
+    this.load()
   }
 
   load() {
-    this.entries = [
-      {
-        login: "fau-33",
-        name: "Flávio Félix",
-        public_repos: "76",
-        followers: "120000",
-      },
-      {
-        login: "diego3g",
-        name: "Diego Fernandes",
-        public_repos: "76",
-        followers: "120000",
-      },
-    ];
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  async add(username) {
+    const user = await GithubUser.search(username)
   }
 
   delete(user) {
-    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+    const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
 
-    console.log(filteredEntries);
+    this.entries = filteredEntries
+    this.update()
   }
 }
 
-// classe que vai criar a visualização e eventos HTML
+// classe que vai criar a visualização e eventos do HTML
 export class FavoritesView extends Favorites {
   constructor(root) {
-    super(root);
+    super(root)
 
-    this.tbody = this.root.querySelector("table tbody");
+    this.tbody = this.root.querySelector('table tbody')
 
-    this.update();
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
   }
 
   update() {
-    this.removeAllTr();
+    this.removeAllTr()
 
-    this.entries.forEach((user) => {
-      const row = this.createRow();
-      row.querySelector(
-        ".user img"
-      ).src = `https://github.com/${user.login}.png`;
-      row.querySelector('.user img').alt = `Imagem de ${user.name}`;
-      row.querySelector('.user p').textContent = user.name;
-      row.querySelector('.user span').textContent = user.login;
-      row.querySelector('.repositories').textContent = user.public_repos;
-      row.querySelector('.followers').textContent = user.followers;   
-      
-      row.querySelector('.remove').onClick = () => {
-       const isOk = confirm('Tem certeza que deletar essa linha?');
-       
-       if(isOk) {
-        this.delete(user)
-       }
+    this.entries.forEach( user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
+        }
       }
 
-      this.tbody.append(row);
-    });
-
-
+      this.tbody.append(row)
+    })
   }
 
   createRow() {
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr')
 
     tr.innerHTML = `
-        
-            <td class="user">
-                <img
-                    src="https://github.com/fau-33.png"
-                    alt="Imagem de Félix Félix"
-                />
-                <a href="https://github.com/fau-33" target="_blank">
-                    <p>
-                    Flávio Félix</p>
-                    <span>fau-33</span>
-                    </p></a
-                >
-                </td>
-                <td class="repositories">
-                76
-                </td>
-                <td class="followers">
-                9589
-                </td>
-                <td>
-                <button class="remove">&times;</button>
-            </td>
-      
-        `;
+      <td class="user">
+        <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
+        <a href="https://github.com/maykbrito" target="_blank">
+          <p>Mayk Brito</p>
+          <span>maykbrito</span>
+        </a>
+      </td>
+      <td class="repositories">
+        76
+      </td>
+      <td class="followers">
+        9589
+      </td>
+      <td>
+        <button class="remove">&times;</button>
+      </td>
+    `
 
-    return tr;
+    return tr
   }
 
   removeAllTr() {
-    this.tbody.querySelectorAll("tr").forEach((tr) => {
-      tr.remove();
-    });
+    this.tbody.querySelectorAll('tr')
+      .forEach((tr) => {
+        tr.remove()
+      })  
   }
 }
--- stage06-javascript-antes-framework/2.github-favorites/js/main.js
+++ stage06-javascript-antes-framework/2.github-favorites/js/main.js
@@ -1,3 +1,3 @@
 import { FavoritesView } from "./Favorites.js";
 
-new FavoritesView("#app");
+new FavoritesView("#app")
