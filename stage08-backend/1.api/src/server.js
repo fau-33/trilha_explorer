2ff6d2ef9ebb3d022f762f1d9b7b89929e2d85e7
Flávio Félix <fau_felix30@hotmail.com>
2023-10-20 21:30:40 -0300

    Organização de arquivos

--- stage08-backend/1.api/src/server.js
+++ stage08-backend/1.api/src/server.js
@@ -1,21 +1,12 @@
 const express = require("express");
 
+const routes  = require("./routes");
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params;
-    
-    response.send(`
-        Messagem ID: ${id}.
-        Para o usuário: ${user}.
-    `);
-});
+app.use(routes);
 
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
-
-    response.send(`Página: ${page}. Mostrar: ${limit}`);
-});
 
 
 const PORT = 3333;
