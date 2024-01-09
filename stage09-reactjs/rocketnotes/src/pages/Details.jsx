006c13c756c5d0ee48350df2954d0a9240df65a1
Flávio Félix <fau_felix30@hotmail.com>
2024-01-09 20:12:25 -0300

    Aplicação do estilo global

--- stage09-reactjs/rocketnotes/src/pages/Details.jsx
+++
@@ -1,3 +0,0 @@
-export function Details() {
-  return <h1>Hello World !</h1>;
-}
---
+++ stage09-reactjs/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello World !</h1>
+    </Container>
+  );
+}
