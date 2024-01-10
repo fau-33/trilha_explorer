09d6697186ab7d6cabec4f8d2e3dff6a40a4fc7c
Flávio Félix <fau_felix30@hotmail.com>
2024-01-09 21:10:47 -0300

    Feito componente

--- stage09-reactjs/rocketnotes/src/pages/Details/index.jsx
+++ stage09-reactjs/rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./style";
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World !</h1>
+      <Button title="Login" loading />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
   );
 }
