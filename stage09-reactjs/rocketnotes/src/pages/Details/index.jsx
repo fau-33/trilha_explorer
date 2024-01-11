68fb1c9b8387efe980d9afc962aefb53a0deb024
Flávio Félix <fau_felix30@hotmail.com>
2024-01-11 14:58:21 -0300

    Feito os links e marcadores da página details

--- stage09-reactjs/rocketnotes/src/pages/Details/index.jsx
+++ stage09-reactjs/rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,29 @@
-import { Container } from "./style";
+import { Container, Links } from "./style";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
 
       <Button title="Voltar" />
     </Container>
